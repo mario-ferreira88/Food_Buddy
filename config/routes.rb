20d593736f33fd6d6e80f3638fc53ccaa04adedc
Cynthia Albuquerque <cynthia.alb25@gmail.com>
2023-11-28 15:06:43 +0000

    profile update and edit done

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
 
-
   root to: "pages#landing"
 
   get "dashboard", to: "pages#dashboard", as: :dashboard
-
-  get "up" => "rails/health#show", as: :rails_health_check
 
   resources :profiles, only: [:create, :new, :edit, :update] do
     collection do
       ## profiles/my_profile
       get :my_profile, to: "profiles#my_profile"
+      patch :my_profile, to: "profiles#update"
+      get :my_profile_edit, to: "profiles#edit"
     end
   end
 end
