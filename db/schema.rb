4e1e5da3d96f2fd33bfdc38681e3b19920c594a4
Sarah Belledent <sarah.belledent@gmail.com>
2023-11-28 15:51:18 +0000

    create event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_163623) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_122518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_163623) do
     t.bigint "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["restaurant_id"], name: "index_events_on_restaurant_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
