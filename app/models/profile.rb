32f06fbbc5b4de10619a6bb575472a304d172bf0
Mario Ferreira <mario.jfs.ferreira@gmail.com>
2023-11-28 14:45:06 +0000

    show changes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,13 +1,10 @@
 class Profile < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
 
   validates :first_name, :last_name, :phone_number, :address, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
   end
-=======
   has_one_attached :photo
->>>>>>> master
 end
