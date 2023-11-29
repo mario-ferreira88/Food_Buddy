5aec369571b63784275c3799919f9a4b51328b54
Mafalda Pires <mfpires111@gmail.com>
2023-11-29 15:26:55 +0000

    some controllers and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_one :profile
+  has_many :user_groups
+  has_many :groups, through: :user_groups
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :profile
 end
