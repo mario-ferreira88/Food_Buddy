49c7df0ed91fbffb61cd7e71d484a03ced3920d7
Mario Ferreira <mario.jfs.ferreira@gmail.com>
2023-11-30 10:36:46 +0000

    some changes to links

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_one :profile
   has_many :user_groups
   has_many :groups, through: :user_groups
+  has_many :user_events
+  has_many :events, through: :user_events
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
