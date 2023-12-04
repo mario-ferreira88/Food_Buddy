426f833be5f0ad5c22d1e32dea7e804f395037dc
Mario Ferreira <mario.jfs.ferreira@gmail.com>
2023-12-04 10:51:19 +0000

    see all the groups

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
-  belongs_to :user
+  has_many :users
   belongs_to :restaurant
+  belongs_to :user
   has_many :group_events, dependent: :destroy
   has_many :groups, through: :group_events
 end
