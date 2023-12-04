6b59ad1ab89d253672f64c2aa9be310d67299baf
Cynthia Albuquerque <cynthia.alb25@gmail.com>
2023-12-04 22:34:38 +0000

    restaurant addresses and categories seeds, and landing pg animation

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,17 @@ addresses = [
   'R. da Rosa 51, 1200-382 Lisboa',
   'R. do Comércio 111, 1100-150 Lisboa',
   'Rua da Prata 242, 1100-052 Lisboa',
-  'R. Dom Pedro V Nº56-D, 1250-094 Lisboa'
+  'R. Dom Pedro V Nº56-D, 1250-094 Lisboa',
+  'R. da Misericórdia 78, 1200-273 Lisboa',
+  'R. da Moeda 1, 1200-109 Lisboa',
+  'R. de São Paulo 99, 1200-429 Lisboa',
+  'R. da Boavista 16, 1200-066 Lisboa',
+  'R. da Oliveira ao Carmo 9, 1200-307 Lisboa',
+  'R. São Boaventura 42, 1200-385 Lisboa',
+  'R. Nova da Piedade 62, 1200-299 Lisboa',
+  'Escolas Gerais 34, 1100-213 Lisboa',
+  'R. Norberto de Araújo 18A, 1100-117 Lisboa',
+  'R. da Misericórdia 71, 1200-365 Lisboa',
 ]
 
 # Generate 15 fake restaurants
@@ -59,3 +69,25 @@ Category.create(name: 'Seafood')
 
 RestaurantCategory.create(restaurant_id: 1, category_id: 1)
 RestaurantCategory.create(restaurant_id: 1, category_id: 2)
+RestaurantCategory.create(restaurant_id: 1, category_id: 10)
+RestaurantCategory.create(restaurant_id: 1, category_id: 4)
+
+RestaurantCategory.create(restaurant_id: 2, category_id: 2)
+RestaurantCategory.create(restaurant_id: 2, category_id: 3)
+RestaurantCategory.create(restaurant_id: 2, category_id: 6)
+RestaurantCategory.create(restaurant_id: 2, category_id: 7)
+
+RestaurantCategory.create(restaurant_id: 3, category_id: 4)
+RestaurantCategory.create(restaurant_id: 3, category_id: 1)
+RestaurantCategory.create(restaurant_id: 3, category_id: 8)
+RestaurantCategory.create(restaurant_id: 3, category_id: 5)
+
+RestaurantCategory.create(restaurant_id: 4, category_id: 3)
+RestaurantCategory.create(restaurant_id: 4, category_id: 7)
+RestaurantCategory.create(restaurant_id: 4, category_id: 8)
+RestaurantCategory.create(restaurant_id: 4, category_id: 9)
+
+RestaurantCategory.create(restaurant_id: 5, category_id: 1)
+RestaurantCategory.create(restaurant_id: 5, category_id: 2)
+RestaurantCategory.create(restaurant_id: 5, category_id: 4)
+RestaurantCategory.create(restaurant_id: 5, category_id: 10)
