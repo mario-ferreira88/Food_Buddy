acb03800366e8b0eb4fc98f1dab4e1a4f54dda98
Sarah Belledent <sarah.belledent@gmail.com>
2023-12-05 22:05:38 +0000

    frontend-harmonization

--- db/migrate/20231204101548_add_type_to_events.rb
+++ db/migrate/20231204101548_add_type_to_events.rb
@@ -3,6 +3,6 @@ class AddTypeToEvents < ActiveRecord::Migration[7.1]
     add_column :events, :type, :string, default: 'Event'
     add_reference :events, :group
 
-    drop_table :group_events
+    # drop_table :group_events
   end
 end
